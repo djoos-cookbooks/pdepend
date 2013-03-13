32eccaa6a67b61bb769645ea9e33886a595b5a70
David Joos <david.joos@gmail.com>
2013-03-13 19:30:16 +0000

    metadata attribute change(s)

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,18 @@
+name             "pdepend"
 maintainer       "Escape Studios"
 maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures pdepend"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
-supports "ubuntu"
-supports "debian"
-supports "centos"
-supports "redhat"
-supports "fedora"
-supports "scientific"
-supports "amazon"
+%w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
+supports os
+end
 
 depends "php"
 depends "composer"
+
+recipe "pdepend", "Installs pdepend."
+recipe "pdepend::composer", "Installs pdepend using composer."
+recipe "pdepend::pear", "Installs pdepend using pear."
